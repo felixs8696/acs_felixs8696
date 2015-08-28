e3bcf5c6bdb6d64743cca1b2c5fc5dd8ff33dc7c
felixs8696 <felixs8696@berkeley.edu>
2015-08-27 23:47:00 -0700

    added hw1 skeleton

---
+++ hw1/hw1.py
@@ -0,0 +1,13 @@
+import doctest
+
+# Calculates a^b using only addition
+def expFromAdd(a,b):
+	"""Calculates a^b using only addition
+    >>> expFromAdd(2,3)
+    8
+    >>> expFromAdd(5,5)
+    3125
+    """
+	return 0
+
+doctest.testmod()
